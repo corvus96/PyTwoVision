f7432b77c431ce1a68432c685f00f3d39e0d916a
corvus96 <guillermoraven96@gmail.com>
2021-08-10 00:22:32 -0400

    feature: add take_photos method

--- pytwovision/input/camera.py
+++ pytwovision/input/camera.py
@@ -2,27 +2,78 @@ import glob
 import os
 import cv2 as cv
 import numpy as np 
+import requests
+import re
 
 class Camera:
-    def __init__(self, identifier):
-        self.id = identifier
-        self.camera_matrix = []
-        self.ret = None
-        self.rvecs = None
-        self.tvecs = None
-        self.dist_coeffs = None
     
-    def take_photos(self, num_photos=15):
+    def __init__(self, id, source):
+        """Build SSD model given a backbone
+        Arguments:
+            id (str): An identifier to our camera.
+            source (int | str): When you use webcam you need to put (int) 0,
+            if you want to use videos or streaming, you will need to put his URL or path.
+        Returns:
+            A model of the real world camera to use in stereo problems 
+        """
+        self.id = id
+        self.source = source
+        # define type of source
+        regex = '((http|https)://)(www.)?' + '[a-zA-Z0-9@:%._\\+~#?&//=]{2,256}\\.[a-z]' + '{2,6}\\b([-a-zA-Z0-9@:%._\\+~#?&//=]*)'
+        regular_exp = re.compile(regex)
+        if type(self.source) is str:
+            if re.search(regular_exp, self.source):
+                self.type_source = 'stream'
+            else:
+                self.type_source = 'other'
+        else:
+            self.type_source = 'webcam'
+
+    def take_photos(self, num_photos=15, save_dir="/temp/root", prefix_name="photo"):
+        """ A simple way to take photos by console and save in a folder
+        Arguments:
+            num_photos (int): Number of photos
+            save_dir ()
+        """
+        # initial directory
+        cwd = os.getcwd()
+        # make directory
+        try:
+            os.mkdir(save_dir)
+            os.chdir(save_dir)
+        except OSError as error:
+            print(error)    
+        # control variables
         stop = False
         event = False
+
+        if self.type_source == 'other' or 'webcam':
+            cap = cv.VideoCapture(self.source)
+            if not cap.isOpened():
+                print("Cannot open camera")
+                exit()
+
         for i in range(num_photos):
             if stop:
                 break
             while event is False:
-                input_key = input("Please press space to take a photo\n")
+                if self.type_source == 'stream':
+                    img_resp = requests.get(self.source)
+                    img_arr = np.array(bytearray(img_resp.content), dtype=np.uint8)
+                    frame = cv.imdecode(img_arr, -1)
+                    cv.imshow("streaming: {}".format(self.id), frame)
+                elif self.type_source == 'other' or 'webcam':
+                    _, frame = cap.read()
+                    cv.imshow("webcam: {}".format(self.id), frame)
+
+                input_key = input("Please press enter to take a photo\n")
                 if input_key == '':
+                    cv.imwrite('{}_{}.png'.format(prefix_name, i), frame)
                     print("{} photos left".format(num_photos - i - 1))
                     break
+        
+        # restore to initial directory
+        os.chdir(cwd)
 
     def calibrate(self, images_path='', pattern_type='chessboard', pattern_size=(7,6)):
         # adjust image path
@@ -57,7 +108,7 @@ class Camera:
                     objpoints.append(objp)
                     corners2 = cv.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
                     imgpoints.append(corners)
-            self.ret, self.camera_matrix, self.dist_coeffs, self.rvecs, self.tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+            ret, matrix, dist_coeffs, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
             
         except OSError:
             print("Could not find any image in {}".format(images_path))
